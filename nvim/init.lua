2f8d490c509efb77d67ee3691cb0319cb38a1bd1
Andrei Dolgov <andrei@dolgov.dev>
2024-12-30 08:45:22 -0500

    [feat] make jest work with debugger as well

--- nvim/init.lua
+++ nvim/init.lua
@@ -962,18 +962,9 @@ require('lazy').setup({
       }
 
       require('mason-nvim-dap').setup {
-        -- Makes a best effort to setup the various debuggers with
-        -- reasonable debug configurations
         automatic_setup = true,
-
-        -- You can provide additional configuration to the handlers,
-        -- see mason-nvim-dap README for more information
         handlers = {},
-
-        -- You'll need to check that you have the required things installed
-        -- online, please don't ask me how to install them :)
         ensure_installed = {
-          -- Update this to ensure that you have the debuggers for the langs you want
           'delve',
         },
       }
@@ -1016,23 +1007,7 @@ require('lazy').setup({
       -- Dap UI setup
       -- For more information, see |:help nvim-dap-ui|
       dapui.setup {
-        -- Set icons to characters that are more likely to work in every terminal.
-        --    Feel free to remove or use ones that you like more! :)
-        --    Don't feel like these are good choices.
         icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
-        controls = {
-          icons = {
-            pause = '⏸',
-            play = '▶',
-            step_into = '⏎',
-            step_over = '⏭',
-            step_out = '⏮',
-            step_back = 'b',
-            run_last = '▶▶',
-            terminate = '⏹',
-            disconnect = '⏏',
-          },
-        },
       }
 
       dap.listeners.after.event_initialized['dapui_config'] = dapui.open
@@ -1041,6 +1016,8 @@ require('lazy').setup({
 
       -- Install golang specific config
       require('dap-go').setup()
+
+      -- Install node specific config
       dap.adapters['pwa-node'] = {
         type = 'server',
         host = '::1',
@@ -1071,8 +1048,10 @@ require('lazy').setup({
             type = 'pwa-node',
             request = 'attach',
             name = 'Attach',
-            processId = require('dap.utils').pick_process,
+            -- processId = require('dap.utils').pick_process,
             cwd = '${workspaceFolder}',
+            address = 'localhost',
+            port = 9229,
             outFiles = {
               '${workspaceFolder}/**/*.(m|c|)js',
               '!**/node_modules/**',
@@ -1081,36 +1060,20 @@ require('lazy').setup({
         }
       end
 
-      -- require('dap-vscode-js').setup {
-      --   adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost', 'node', 'chrome' },
-      --   -- debugger_path = '/usr/lib/js-debug/',
-      --   -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
-      --   -- debugger_path = "(runtimedir)/site/pack/packer/opt/vscode-js-debug", -- Path to vscode-js-debug installation.
-      --   debugger_cmd = { 'js-debug-adapter' }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
-      --   -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
-      --   -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
-      --   -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
+      -- dap.configurations[''] = {
+      --   {
+      --     type = 'pwa-node',
+      --     request = 'attach',
+      --     name = 'Attach to Nest.js',
+      --     cwd = '${workspaceFolder}',
+      --     address = 'localhost',
+      --     port = 9229,
+      --     outFiles = {
+      --       '${workspaceFolder}/**/*.(m|c|)js',
+      --       '!**/node_modules/**',
+      --     },
+      --   },
       -- }
-      --
-      -- local langs = { 'javascript', 'typescript' }
-      -- for _, lang in ipairs(langs) do
-      --   dap.configurations[lang] = {
-      --     {
-      --       type = 'pwa-node',
-      --       request = 'attach',
-      --       name = 'Attach to Node app',
-      --       address = 'localhost',
-      --       cwd = '${workspaceFolder}',
-      --     },
-      --     {
-      --       type = 'pwa-node',
-      --       request = 'attach',
-      --       name = 'Attach 2',
-      --       processId = require('dap.utils').pick_process,
-      --       cwd = '${workspaceFolder}',
-      --     },
-      --   }
-      -- end
 
       local elixir_ls_debugger = vim.fn.exepath 'elixir-ls-debugger'
       if elixir_ls_debugger ~= '' then
@@ -1159,6 +1122,10 @@ require('lazy').setup({
             cwd = function()
               return vim.fn.getcwd()
             end,
+            outFiles = {
+              '${workspaceFolder}/**/*.(m|c|)js',
+              '!**/node_modules/**',
+            },
           },
           require 'neotest-go',
         },
