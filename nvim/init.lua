9ccccc15d7e7f8aa752f9993d869fc618aa626ba
Andrei Dolgov <andrei@dolgov.dev>
2025-02-10 20:18:18 -0500

    feat: a few improvements

--- nvim/init.lua
+++ nvim/init.lua
@@ -649,6 +649,18 @@ require('lazy').setup({
       require('copilot').setup {}
     end,
   },
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    dependencies = {
+      { 'zbirenbaum/copilot.lua' },
+      { 'nvim-lua/plenary.nvim', branch = 'master' },
+    },
+    build = 'make tiktoken', -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
 
   { 'onsails/lspkind.nvim' },
 
